0187b79129846be593969390a8e04ff70cd64679
kai.inoue <kai.inoue@nobinobito.co.jp>
2026-06-20 16:21:19 +0900

    fix: LCP Font weight削減

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -3,14 +3,14 @@ import localFont from "next/font/local";
 
 export const notoSansJp = Noto_Sans_JP({
   subsets: ["latin"],
-  weight: ["400", "500", "700"],
+  weight: ["400"],
   variable: "--font-noto-sans-jp",
   display: "swap",
 });
 
 export const barlowCondensed = Barlow_Condensed({
   subsets: ["latin"],
-  weight: ["400", "500", "600"],
+  weight: ["400"],
   variable: "--font-barlow-condensed",
   display: "swap",
 });
